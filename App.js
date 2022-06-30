4ba5fafe2f258b0f0ec78312c1d340dae065b31c
eslam <eslamfgazy600@gmail.com>
2022-06-30 03:02:03 +0200

    Update Ui

--- App.js
+++ App.js
@@ -4,7 +4,13 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function App() {
     return (
         <View>
-            <View style={{padding: 50, flexDirection: 'row', width: '80%', height: 300}}>
+            <View style={{
+                paddingTop: 50,
+                flexDirection: 'row',
+                width: '80%',
+                height: 300,
+                alignItems: 'center'
+            }}>
                 <View style={styles.red}>
                     <Text>1</Text>
                 </View>
@@ -42,19 +48,22 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         backgroundColor: '#ff0000',
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: 'center', // center in  vertical
+        justifyContent: 'center', // center in horizontal
+        flex: 1
     },
-    green: {
-        width: 100,
-        height: 100,
+    blue: {
+        // width: 100,
+        // height: 100,
         backgroundColor: '#0034ff',
         alignItems: 'center',
         justifyContent: 'center',
+        flex: 2
+
     },
-    blue: {
-        width: 100,
-        height: 100,
+    green: {
+        // width: 100,
+        // height: 100,
         backgroundColor: '#66ff00',
         alignItems: 'center',
         justifyContent: 'center',
